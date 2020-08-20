7a98a386b7cad47d317e5f03f1c3631c6728536f
Matt Perry <mpcomplete@gmail.com>
2020-08-20 12:53:32 -0400

    2 teams

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,6 +1,12 @@
 ﻿using UnityEngine;
 
+public enum PlayerTeam {
+  One = 1,
+  Two = 2,
+}
+
 public class Player : MonoBehaviour {
+  public PlayerTeam Team = PlayerTeam.One;
   public float Speed = 1f;
   CharacterController controller;
   private void Start() {
@@ -10,10 +16,18 @@ public class Player : MonoBehaviour {
   // Update is called once per frame
   void Update() {
     Vector2 movement = Vector2.zero;
-    if (Input.GetKey(KeyCode.W)) movement += Vector2.up;
-    if (Input.GetKey(KeyCode.A)) movement += Vector2.left;
-    if (Input.GetKey(KeyCode.S)) movement += Vector2.down;
-    if (Input.GetKey(KeyCode.D)) movement += Vector2.right;
+    if (Team == PlayerTeam.One) {
+      if (Input.GetKey(KeyCode.W)) movement += Vector2.up;
+      if (Input.GetKey(KeyCode.A)) movement += Vector2.left;
+      if (Input.GetKey(KeyCode.S)) movement += Vector2.down;
+      if (Input.GetKey(KeyCode.D)) movement += Vector2.right;
+    } else {
+      if (Input.GetKey(KeyCode.UpArrow)) movement += Vector2.up;
+      if (Input.GetKey(KeyCode.LeftArrow)) movement += Vector2.left;
+      if (Input.GetKey(KeyCode.DownArrow)) movement += Vector2.down;
+      if (Input.GetKey(KeyCode.RightArrow)) movement += Vector2.right;
+    }
+
     if (movement != Vector2.zero) {
       movement = movement.normalized * Speed * Time.deltaTime;
       controller.Move(new Vector3(movement.x, 0, movement.y));
