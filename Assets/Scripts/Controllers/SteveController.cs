eb3f6071ba577676099b50bb832303925793072b
Steve Kane <kanesteven@gmail.com>
2020-08-20 17:58:29 -0500

    Removed old tracking gizmos from SteveControllre

--- Assets/Scripts/Controllers/SteveController.cs
+++ Assets/Scripts/Controllers/SteveController.cs
@@ -20,23 +20,4 @@ public class SteveController : MonoBehaviour {
     BaseSystem.Update(Team1, dt);
     BaseSystem.Update(Team2, dt);
   }
-
-  void OnDrawGizmos() {
-    void RenderFightStatus(List<Minion> minions, Player player) {
-      foreach (var minion in minions) {
-        bool inRangeToFight = Vector3.Distance(minion.transform.position, player.transform.position) <= minion.FightRadius;
-
-        if (inRangeToFight) {
-          Gizmos.color = Color.red;
-          Gizmos.DrawLine(minion.transform.position, player.transform.position);
-        } else {
-          Gizmos.color = Color.yellow;
-          Gizmos.DrawWireSphere(minion.transform.position, minion.FightRadius);
-        }
-      }
-    }
-
-    RenderFightStatus(Team1.Minions, Team2.Player);
-    RenderFightStatus(Team2.Minions, Team1.Player);
-  }
 }
