a64883d3836178ccaab78691fc0dc1e452625e66
Steve Kane <kanesteven@gmail.com>
2020-08-21 23:13:31 -0500

    Added scroll to water

---
+++ Assets/Scripts/MonoBehaviors/UVScroller.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class UVScroller : MonoBehaviour {
+  public Vector2 Rate;
+
+  public Vector2 Offset;
+  [SerializeField] MeshRenderer MeshRenderer;
+
+  public void Update() {
+    Offset += Rate * Time.deltaTime;
+    MeshRenderer.material.mainTextureOffset = Offset;
+  }
+}
