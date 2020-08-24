5829a07636db178033525d4df09471f9d633cadc
Steve Kane <kanesteven@gmail.com>
2020-08-24 11:12:08 -0500

    terrain experiments consolidated

---
+++ Assets/DOTS Experiments/Scripts/LittleWanderer.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+public class LittleWanderer : MonoBehaviour {
+  public NavMeshAgent NavMeshAgent;
+  public Transform Destination;
+
+  public void Start() {
+    NavMeshAgent.SetDestination(Destination.position);
+  }
+}
