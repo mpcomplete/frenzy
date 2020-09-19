cdef897f000314345b4c9ebacda18561ddabd856
Steve Kane <kanesteven@gmail.com>
2020-09-18 20:35:53 -0500

    Added experimental version of DOTS editor

--- Assets/ECS Frenzy/Scripts/Components/Cooldown.cs
+++ Assets/ECS Frenzy/Scripts/Components/Cooldown.cs
@@ -12,5 +12,10 @@ namespace ECSFrenzy {
       ecb.SetComponent<Cooldown>(nativeThreadIndex, e, new Cooldown { Duration = cd.Duration, TimeRemaining = cd.Duration });
       ecb.SetSharedComponent<SharedCooldownStatus>(nativeThreadIndex, e, SharedCooldownStatus.JustActive);
     }
+
+    public static void Reset(EntityCommandBuffer.ParallelWriter ecb, Entity e, int nativeThreadIndex, Cooldown cd) {
+      ecb.SetComponent<Cooldown>(nativeThreadIndex, e, new Cooldown { Duration = cd.Duration, TimeRemaining = 0 });
+      ecb.SetSharedComponent<SharedCooldownStatus>(nativeThreadIndex, e, SharedCooldownStatus.JustElapsed);
+    }
   }
 }
