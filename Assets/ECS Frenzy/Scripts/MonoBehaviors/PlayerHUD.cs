8ed7aad34417b23669468e53513b40d8e5e914e5
Steve Kane <kanesteven@gmail.com>
2020-09-25 16:58:40 -0500

    added basic immediate mode GUI for fireball cooldown and a notification for fireball ability becoming available

--- Assets/ECS Frenzy/Scripts/MonoBehaviors/PlayerHUD.cs
+++ Assets/ECS Frenzy/Scripts/MonoBehaviors/PlayerHUD.cs
@@ -1,18 +1,67 @@
-﻿using UnityEngine;
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
 
 namespace ECSFrenzy {
   public class PlayerHUD : MonoBehaviour {
+    [Serializable]
+    public class AbilityInfo {
+      public string Name;
+      public float TimeRemaining;
+      public float Duration;
+    }
+
+    [Serializable]
+    public class Notification {
+      public float TimeRemaining;
+      public string Message;
+
+      public Notification(float duration, string message) {
+        TimeRemaining = duration;
+        Message = message;
+      }
+    }
+
     public Camera Camera;
     public Vector3 PlayerPosition;
-    public float TimeRemaining;
-    public float LabelWidth = 100;
-    public float LabelHeight = 24;
+    public AbilityInfo Ability1 = new AbilityInfo { Name = "Fireball" };
+    public Vector2 AbilityScreenOffsetX;
+    public float AbilityLabelWidth = 20;
+    public float AbilityLabelHeight = 100;
+    public float NotificationWidth = 200;
+    public float NotificationHeight = 24;
+    public float NotificationDuration = 1;
+    public float NotificationDriftHeight = 100;
+    public List<Notification> Notifications = new List<Notification>();
 
     void OnGUI() {
-      var screenPosition = Camera.WorldToScreenPoint(PlayerPosition);
-      var rect = new Rect(screenPosition.x, Camera.pixelHeight - screenPosition.y, LabelWidth, LabelHeight);
+      void GUICooldownMeter(AbilityInfo a, Color bgc, Color mc, float x, float y, float w, float h) {
+        var cooldownBackgroundRect = new Rect(x, y, w, h);
+        var originalColor = GUI.backgroundColor;
 
-      GUI.Label(rect, $"{TimeRemaining}");
+        GUI.backgroundColor = bgc;
+        GUI.Box(cooldownBackgroundRect, GUIContent.none);
+
+        if (a.TimeRemaining > 0) {
+          var fraction = a.TimeRemaining / a.Duration;
+          var meterRect = new Rect(x, y, w, fraction * h);
+
+          GUI.backgroundColor = mc;
+          GUI.Box(meterRect, GUIContent.none);
+        }
+        GUI.backgroundColor = originalColor;
+      }
+
+      Vector2 screenPosition = (Vector2)Camera.WorldToScreenPoint(PlayerPosition) + AbilityScreenOffsetX;
+
+      GUICooldownMeter(Ability1, Color.grey, Color.white, screenPosition.x, Camera.pixelHeight - screenPosition.y, AbilityLabelWidth, AbilityLabelHeight);
+
+      foreach (var notification in Notifications) {
+        var yOffset = NotificationDriftHeight * (1 - notification.TimeRemaining / NotificationDuration);
+        var notificationsRect = new Rect(Camera.pixelWidth / 2, Camera.pixelHeight / 2 - yOffset, NotificationWidth, NotificationHeight);
+
+        GUI.Label(notificationsRect, notification.Message);
+      }
     }
   }
 }
--- Assets/ECS Frenzy/Scripts/Systems/Client/PlayerHUDRenderingSystem.cs
+++ Assets/ECS Frenzy/Scripts/Systems/Client/PlayerHUDRenderingSystem.cs
@@ -3,6 +3,7 @@ using Unity.Transforms;
 using Unity.Jobs;
 using Unity.NetCode;
 using UnityEngine;
+using static Unity.Mathematics.math;
 
 namespace ECSFrenzy {
   [UpdateInGroup(typeof(ClientPresentationSystemGroup))]
@@ -23,6 +24,7 @@ namespace ECSFrenzy {
 
     protected override void OnUpdate() {
       var hudPrefab = SystemConfig.Instance.PlayerHUDPrefab;
+      var dt = Time.DeltaTime;
 
       Entities
       .WithName("Instantiate_PlayerHUD_Instances")
@@ -50,12 +52,24 @@ namespace ECSFrenzy {
 
       Entities
       .WithName("Render_Player_HUD")
-      .WithAll<NetworkPlayer>()
+      .WithAll<NetworkPlayer, PlayerInput>()
       .ForEach((Entity entity, PlayerHUDInstance playerHUDInstance, ref PlayerAbilites abilities, ref Translation position) => {
-        var ability1TimeRemaining = EntityManager.GetComponentData<Cooldown>(abilities.Ability1).TimeRemaining;
+        var ability1Cooldown = EntityManager.GetComponentData<Cooldown>(abilities.Ability1);
+        var ability1Status = EntityManager.GetComponentData<CooldownStatus>(abilities.Ability1);
+        var notifications = playerHUDInstance.Value.Notifications;
 
         playerHUDInstance.Value.PlayerPosition = position.Value;
-        playerHUDInstance.Value.TimeRemaining = ability1TimeRemaining;
+        playerHUDInstance.Value.Ability1.TimeRemaining = ability1Cooldown.TimeRemaining;
+        playerHUDInstance.Value.Ability1.Duration = ability1Cooldown.Duration;
+        for (int i = notifications.Count - 1; i >= 0; i--) {
+          notifications[i].TimeRemaining = max(0, notifications[i].TimeRemaining - dt);
+          if (notifications[i].TimeRemaining <= 0) {
+            notifications.RemoveAt(i);
+          }
+        }
+        if (ability1Status.Value == CooldownStatus.Status.JustElapsed) {
+          playerHUDInstance.Value.Notifications.Add(new PlayerHUD.Notification(playerHUDInstance.Value.NotificationDuration, "Fireball Ready"));
+        }
       })
       .WithoutBurst()
       .Run();
