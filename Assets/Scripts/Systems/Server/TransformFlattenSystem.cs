eae251d665cf64ae9b9382c7278f2d3272ca52f5
Matt Perry <mpcomplete@gmail.com>
2020-10-20 14:51:39 -0400

    Fix ghost transforms by making them toplevel

---
+++ Assets/Scripts/Systems/Server/TransformFlattenSystem.cs
@@ -0,0 +1,22 @@
+﻿using Unity.Entities;
+using Unity.Transforms;
+using Unity.NetCode;
+
+// This dumb system reparents every object to be toplevel, fixing up its transform as it goes.
+//
+// This is hack EXISTS to fix the following issue: server-side ghosts that have non-ghost parents
+// do not send their LocalToWorld data - only their local Translation and Rotation. The client
+// would then render the ghost at the incorrect local transform, instead of the global transform.
+//
+// This converts every object to top-level, so that an objects local transform and global transform
+// are the same, side-stepping the issue once and for all.
+[UpdateInGroup(typeof(ServerSimulationSystemGroup))]
+public class TransformFlattenSystem : ComponentSystem {
+  protected override void OnUpdate() {
+    Entities.ForEach((Entity e, ref Parent parent, ref Translation translation, ref Rotation rotation, ref LocalToWorld transform) => {
+      parent.Value = Entity.Null;
+      translation.Value = transform.Position;
+      rotation.Value = transform.Rotation;
+    });
+  }
+}
